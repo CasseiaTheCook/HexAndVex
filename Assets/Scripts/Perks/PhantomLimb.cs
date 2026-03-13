af4c8f4dd79641ed2cfd67d488865ef5e498d06b
CasseiaTheCook <beserahmetabdullah@gmail.com>
2026-03-13 03:20:11 +0300

    s

--- Assets/Scripts/Perks/PhantomLimb.cs
+++ Assets/Scripts/Perks/PhantomLimb.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class PhantomLimbPerk : BasePerk
 {
     // Mine offset hard-coded
-    private Vector3 mineOffset = new Vector3(-0.07f, 0f, 0f); // X: 0.5, Y: 0, Z: 0
+    private Vector3 mineOffset = new Vector3(0f, -0.07f, 0f); // X: 0.5, Y: 0, Z: 0
     
     void OnEnable()
     {
